c9680d76aa8a1a45939f3f85fbeb885145465a79
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-01-11 15:40:30 -0500

    chore: base

---
+++ constants/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    background_primary: "#121212",
+    background_secondary: "#181818",
+    surface_cards: "#1E1E1E",
+    surface_hover: "#252525",
+    border: "#2A2A2A",
+
+    text_color: "#e6e6e6",
+    primary_accent: "#3d5e95",
+  }
+};
+
+export type Theme = typeof theme;
